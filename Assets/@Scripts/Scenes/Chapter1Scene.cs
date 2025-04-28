2049cdce54d64556c06a2737363c008f71349203
seung <88180714+joseunghui@users.noreply.github.com>
2025-04-29 00:07:40 +0900

    Creature 생성 직전!!!

--- Assets/@Scripts/Scenes/Chapter1Scene.cs
+++ Assets/@Scripts/Scenes/Chapter1Scene.cs
@@ -9,6 +9,11 @@ public class Chapter1Scene : BaseScene
         if (base.Init() == false)
             return false;
 
+        // Map 생성
+        GameObject map = Managers.Resource.Instantiate("Chapter1Map");
+        map.transform.position = Vector3.zero;
+        map.name = "@Chapter1Map";
+
         // TODO Rat 스폰
 
         return true;
